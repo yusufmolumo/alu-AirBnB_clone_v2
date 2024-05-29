6c6f1c23e2a849f7abc285d15dfc9809b59d34a9
yusufmolumo <144047887+yusufmolumo@users.noreply.github.com>
2024-05-29 10:01:45 -0700

    Update test_user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,34 +1,41 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+"""Unit tests for User class"""
+
+from tests.test_models.test_base_model import TestBaseModel
 from models.user import User
 
-
-class test_User(test_basemodel):
-    """ """
+class TestUser(TestBaseModel):
+    """Test User class"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """Initialize TestUser instance"""
         super().__init__(*args, **kwargs)
         self.name = "User"
         self.value = User
 
-    def test_first_name(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.first_name), str)
+    def test_first_name_attribute(self):
+        """Test setting the first_name attribute of User"""
+        new_user = self.value()
+        new_user.first_name = "Chyna"
+        self.assertEqual(type(new_user.first_name), str)
 
-    def test_last_name(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.last_name), str)
+    def test_last_name_attribute(self):
+        """Test setting the last_name attribute of User"""
+        new_user = self.value()
+        new_user.last_name = "Chyna"
+        self.assertEqual(type(new_user.last_name), str)
 
-    def test_email(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.email), str)
+    def test_email_attribute(self):
+        """Test setting the email attribute of User"""
+        new_user = self.value()
+        new_user.email = "angoyewally@gmail.com"
+        self.assertEqual(type(new_user.email), str)
 
-    def test_password(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.password), str)
+    def test_password_attribute(self):
+        """Test setting the password attribute of User"""
+        new_user = self.value()
+        new_user.password = "123aashja"
+        self.assertEqual(type(new_user.password), str)
+
+if __name__ == '__main__':
+    unittest.main()
