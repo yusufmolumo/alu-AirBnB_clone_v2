7040e9be76885188723fc6e518523e8652ab8177
yusufmolumo <144047887+yusufmolumo@users.noreply.github.com>
2024-05-29 09:48:23 -0700

    Update setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,9 @@
--- Prepares a MySQL Test server for the project.
+-- Creates the database hbnb_test_db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+-- add the user hbnb_test identified with password 'hbnb_test_pwd'
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
+-- add privileges to hbnb_test
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+-- refresh to apply privileges
+-- FLUSH PRIVILEGES;
