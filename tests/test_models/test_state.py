722c565365582a8edca471d074aca198b6dd01ef
yusufmolumo <144047887+yusufmolumo@users.noreply.github.com>
2024-05-29 10:02:04 -0700

    Update test_state.py

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+"""Unit tests for State class"""
+
+import unittest
+from tests.test_models.test_base_model import TestBaseModel
 from models.state import State
 
 
-class test_state(test_basemodel):
-    """ """
+class TestState(TestBaseModel):
+    """Test State class"""
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "State"
-        self.value = State
+    def test_name_property(self):
+        """Test the 'name' property of State"""
+        new_state = State()
+        new_state.name = "Arizona"
+        self.assertEqual(type(new_state.name), str)
 
-    def test_name3(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
+if __name__ == '__main__':
+    unittest.main()
