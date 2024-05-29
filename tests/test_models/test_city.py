4a98d6c0d4b2d9ed6c321e36b16a8a82a99fa663
yusufmolumo <144047887+yusufmolumo@users.noreply.github.com>
2024-05-29 10:03:21 -0700

    Update test_city.py

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,24 +1,31 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+"""Unit tests for City class"""
+
+from models.state import State
+from tests.test_models.test_base_model import TestBaseModel
 from models.city import City
 
-
-class test_City(test_basemodel):
-    """ """
+class TestCity(TestBaseModel):
+    """Test City class"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """Initialize TestCity instance"""
         super().__init__(*args, **kwargs)
         self.name = "City"
         self.value = City
 
-    def test_state_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.state_id), str)
+    def test_state_id_attribute(self):
+        """Test setting the state_id attribute of City"""
+        state = State()
+        new_city = self.value()
+        new_city.state_id = state.id
+        self.assertEqual(type(new_city.state_id), str)
 
-    def test_name(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
+    def test_name_attribute(self):
+        """Test setting the name attribute of City"""
+        new_city = self.value()
+        new_city.name = "Batch"
+        self.assertEqual(type(new_city.name), str)
+
+if __name__ == '__main__':
+    unittest.main()
