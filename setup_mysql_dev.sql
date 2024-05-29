02881f56f972b1529bd704b8076842f8bc55aee5
yusufmolumo <144047887+yusufmolumo@users.noreply.github.com>
2024-05-29 09:47:53 -0700

    Update setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,9 @@
--- Prepares a MySQL server for the project.
+-- Creates the database hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- add the user hbnb_dev identified with password 'hbnb_dev_pwd'
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema . * TO 'hbnb_dev'@'localhost';
+-- add privileges to hbnb_dev
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+-- refresh to apply privileges
+-- FLUSH PRIVILEGES;
