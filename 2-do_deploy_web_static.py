bf2179437ed6efa41f4db9d1663b71aafa6ab0c3
yusufmolumo <144047887+yusufmolumo@users.noreply.github.com>
2024-06-06 04:18:07 -0700

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,64 +1,60 @@
 #!/usr/bin/python3
 # script that distributes an archive to web servers
-from fabric.api import env, put, run, local
-from os.path import exists, isdir
-import os.path
+from fabric.api import env, put, run, sudo
+from os.path import exists, isdir, basename, splitext
 import re
-
 
 # Set the username and host for SSH connection to the server
 env.user = 'ubuntu'
-env.hosts = ['54.144.99.190', '54.160.126.226']
+env.hosts = ['54.207.225.125', '54.160.166.220']
 env.key_filename = '~/.ssh/id_rsa'
-
 
 def do_deploy(archive_path):
     """
-        Distributes archive to web servers
+    Distributes archive to web servers
     """
-    # Check if the archive file exists
     if not exists(archive_path):
         return False
 
-    # Upload the archive to the /tmp/ directory of the web server
-    put(archive_path, "/tmp/")
+    try:
+        # Extract the file name without extension
+        filename = basename(archive_path)
+        no_ext = splitext(filename)[0]
+        folder = "/data/web_static/releases/{}".format(no_ext)
 
-    # Uncompress the archive to the folder
-    filename = re.search(r'[^/]+$', archive_path).group(0)
-    folder = "/data/web_static/releases/{}".format(
-        os.path.splitext(filename)[0])
+        # Upload the archive to the /tmp/ directory of the web server
+        put(archive_path, "/tmp/")
 
-    # Create the folder if it doesn't exist
-    if not exists(folder):
+        # Create the release folder if it doesn't exist
         run("mkdir -p {}".format(folder))
 
-    # Extract files from archive
-    run("tar -xzf /tmp/{} -C {}".format(filename, folder))
+        # Uncompress the archive to the folder
+        run("tar -xzf /tmp/{} -C {}".format(filename, folder))
 
-    # Remove archive from web server
-    run("rm /tmp/{}".format(filename))
+        # Remove the archive from the web server
+        run("rm /tmp/{}".format(filename))
 
-    # Move all files from web_static to the new folder
-    run("mv {}/web_static/* {}".format(folder, folder))
+        # Move all files from web_static to the new folder
+        run("mv {}/web_static/* {}".format(folder, folder))
 
-    # Remove the web_static folder
-    run("rm -rf {}/web_static".format(folder))
+        # Remove the web_static folder
+        run("rm -rf {}/web_static".format(folder))
 
-    # Delete the symbolic link
-    run("rm -rf /data/web_static/current")
+        # Delete the old symbolic link
+        run("rm -rf /data/web_static/current")
 
-    # Create new symbolic link
-    run("ln -s {} /data/web_static/current".format(folder))
+        # Create a new symbolic link
+        run("ln -s {} /data/web_static/current".format(folder))
 
-    # Create 'hbnb_static' directory if it doesn't exist
-    if not isdir("/var/www/html/hbnb_static"):
+        # Ensure the 'hbnb_static' directory exists and sync with 'current'
         run("sudo mkdir -p /var/www/html/hbnb_static")
+        run("sudo cp -r /data/web_static/current/* /var/www/html/hbnb_static/")
 
-    # Sync 'hbnb_static' with 'current'
-    run("sudo cp -r /data/web_static/current/* /var/www/html/hbnb_static/")
-
-    print("New version deployed!")
-    return True
+        print("New version deployed!")
+        return True
+    except Exception as e:
+        print(e)
+        return False
 
 # Usage:
 # fab -f 2-do_deploy_web_static.py do_deploy:/path/to/file.tgz
